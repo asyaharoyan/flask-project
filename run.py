74595b5abcb6d57eab89594454687a02ee349e16
Asya Haroyan <alharoyan@gmail.com>
2024-02-04 19:43:10 +0100

    Add env.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 """Module providing a function printing python version."""
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 
@@ -31,12 +31,15 @@ def about_member(member_name):
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-    return "<h1>" + member["name"] + "</h1>"
+    return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
     """Open the contact page"""
+    if request.method == ("POST"):
+        print(request.form.get("name"))
+        print(request.form["email"])
     return render_template("contact.html", page_title="Contact")
 
 
