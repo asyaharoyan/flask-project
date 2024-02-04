9dd2d3f25566edfa89cc3c1f9a7a71d8d9ac5ad7
Asya Haroyan <alharoyan@gmail.com>
2024-02-04 20:17:58 +0100

    Finish the project

--- run.py
+++ run.py
@@ -2,8 +2,12 @@
 import os
 import json
 from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -38,8 +42,7 @@ def about_member(member_name):
 def contact():
     """Open the contact page"""
     if request.method == ("POST"):
-        print(request.form.get("name"))
-        print(request.form["email"])
+        flash("Thanks {}, we have received your message!".format(request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
