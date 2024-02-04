720e3f724d320f6a455cf07688e89dc1df055d98
Asya Haroyan <alharoyan@gmail.com>
2024-02-04 18:29:13 +0100

    Add json file and render to the HTML file

--- run.py
+++ run.py
@@ -15,7 +15,23 @@ def index():
 @app.route("/about")
 def about():
     """Open the about page"""
-    return render_template("about.html", page_title="About", list_of_numbers=[1, 2, 3])
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    """
+    Open the about link
+    """
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return "<h1>" + member["name"] + "</h1>"
 
 
 @app.route("/contact")
